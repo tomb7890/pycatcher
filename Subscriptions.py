4d2a4b78699337bc238bfeb25d8dd77a4b8ac15c
Tom Billingsley <tom.billingsley@gmail.com>
2014-04-17 08:41:51 -0400

    a number of refactorings

--- Subscriptions.py
+++ Subscriptions.py
@@ -2,20 +2,19 @@ import os
 
 class Subscription:
     def __init__(self, s):
+        self.subscriptions = s 
         self.xmlfile = None
         self.url = None
         self.maxeps = None
         self.dir = None
-        self.subscriptions = s 
 
 class Subscriptions: 
     def __init__(self, b=None):
         """A subscriptions file ("podcasts.ini") is a user defined file,
         it allows the specification of the podcast programs to be downloaded. 
 
-        It contains a number of lines, each of which represent a
-        podcast subscription. A given subscription line has a number
-        of fields, separated by commas.
+        Each line in the file describes a podcast subscription. Each line
+        is made up of fields separated by commas. 
 
         The first field is the filename of a program's downloaded RSS
         (XML) file to be parsed. The second field is the URL for updates. 
@@ -23,16 +22,22 @@ class Subscriptions:
         to keep at a time.
 
         """
-        # open an ini file and parse into lines 
-        self.basedir=b
-        f = open(os.path.join(self.basedir, 'podcasts.ini'), 'r')
-        data = f.read()
-        self.lines = data.split('\n')
 
-        # the directory of the target podcast (not full/absolute) 
         self.items = []
         self.doomed = []
+        self.basedir=b
 
+        self.parse_file()
+
+    def parse_file (self ):
+        """ Parse a podcasts.ini file into lines """
+        try:
+            f = open(os.path.join(self.basedir, 'podcasts.ini'), 'r')
+        except IOError, err:
+            # print "can't find subscriptions file"
+            raise err
+        data = f.read()
+        self.lines = data.split('\n')
         for l in self.lines:
             fields = l.split(',')
             if ( len( fields ) > 1 ):
@@ -41,30 +46,24 @@ class Subscriptions:
                     self.items.append( pc )
 
     def parse_line(self, fields):
-        dir = ''
-        maxeps = 3
-        url = ''
-
+        """ parse_line decodes a filename, a subscription url, and a maximum number of episodes """
         xmlfile = fields[0].strip()
         if xmlfile.startswith("#"):
             return None
         if xmlfile.endswith(".xml"):
             dir = xmlfile[:-4]
-
         url = fields[1].strip()
-
+        maxeps=3 # default to 3 
         if ( len ( fields ) > 2 ):
             n = fields[2].strip()
             maxeps = n
-
         pc = Subscription(self)
-        pc.dir = dir
         pc.xmlfile = xmlfile
-        pc.maxeps = int(maxeps)
         pc.url = url
+        pc.dir = dir
+        pc.maxeps = int(maxeps)
         return pc
 
-    
     def numtokeep( self, program ):
         for pc in self.items:
             if pc.dir == program:
