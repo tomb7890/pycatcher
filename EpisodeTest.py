0c6ca5d695aef184cb8d7844a6acccfa6126de38
Tom Billingsley <tom.billingsley@gmail.com>
2016-02-07 15:44:27 -0500

    consolidate loose code into helper method

--- EpisodeTest.py
+++ EpisodeTest.py
@@ -2,9 +2,11 @@ import unittest, os
 from Episode import Episode
 from Subscriptions import Subscriptions
 
+from ApplicationTest import helper
+
 class EpisodeTest (unittest.TestCase):
     def setUp(self):
-        self.standardpath = os.environ['PODCASTROOT']
+        self.standardpath = helper()
 
     def test_episodes(self):
         pass
