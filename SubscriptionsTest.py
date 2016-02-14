171c15844a7b752512fef1746fd3e068bd1352ad
Tom Billingsley <tom.billingsley@gmail.com>
2016-02-14 08:41:01 -0500

    initial revision of Subscriptions tests

---
+++ SubscriptionsTest.py
@@ -0,0 +1,49 @@
+import unittest
+from Application import init_config, get_list_of_subscriptions
+import tempfile
+import xml
+
+
+class SubscriptionsTest (unittest.TestCase):
+    def setUp(self):
+        self.standardpath = init_config()
+
+    def test_junk_in_header(self):
+        '''test processing an RSS file with junk at the top of the header'''
+        s = self.set_up_minidom_test()
+        temp = tempfile.mktemp()
+        f = open(temp, 'w')
+        f.write('blah blah blah blah blah')
+        partial = open(s.get_rss_path(), 'r').read()
+        f.write(partial)
+        f.close()
+
+        try:
+            s.minidom_parse(temp)
+        except xml.parsers.expat.ExpatError:
+            self.assertTrue(True)
+
+    def test_empty_rss_file(self):
+        '''test processing an empty RSS file'''
+        s = self.set_up_minidom_test()
+        temp = tempfile.mktemp()
+        f = open(temp, 'w')
+        f.write('')
+        f.close()
+
+        try:
+            s.minidom_parse(temp)
+        except xml.parsers.expat.ExpatError:
+            self.assertTrue(True)
+
+    def set_up_minidom_test(self):
+        basedir = self.standardpath
+        s = get_list_of_subscriptions(basedir)[0]
+        return s
+
+    def test_get_rss_file(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
