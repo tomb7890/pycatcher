9bfae0371863a9e12a87b78f94decf49a15160ff
Tom Billingsley <tom.billingsley@gmail.com>
2017-03-19 16:32:45 -0400

    Test with fake episode data

--- DuplicatesTest.py
+++ DuplicatesTest.py
@@ -1,8 +1,8 @@
-import unittest, os
+import unittest
+import os
 from Application import init_config
-from Subscriptions import Subscriptions
-from Filesystem import FileSystem
-from LookupTable import LookupTable
+from Subscriptions import Subscriptions, FakeSubscription
+from Episode import Episode
 
 
 class DuplicatesTest(unittest.TestCase):
@@ -11,20 +11,22 @@ class DuplicatesTest(unittest.TestCase):
         self.standardpath = init_config()
         subscriptions = Subscriptions(self.standardpath)
         self.sub = subscriptions.find("Agenda")
-        self.sub.lut = LookupTable(self.sub.get_idx_path())
         self.episodes = self.sub.get_all_episodes()
         self.gather_dupe_indices()
 
-    def test_number_not_append(self):
-        index_of_folower_of_first_dupe = self.dupes[0]+1
-        first_follower = self.episodes[index_of_folower_of_first_dupe]
-        self.assertEqual("Ontarios Pothole Pains.mp4",
-                         first_follower.basename())
+    def xassertEqual(self, a, b):
+        self.assertEqual(os.path.join(self.sub._podcasts_subdir(), a), b)
 
-    def g_test_number_appended(self):
+    def test_number_not_append(self):
+        index_of_folower_of_first_dupe = self.dupes[1]+1
+        first_follower = self.episodes[index_of_folower_of_first_dupe]
+        self.xassertEqual("Ontarios Pothole Pains.mp4",
+                          first_follower.locallink())
+
+    def test_number_appended(self):
         third_duplicate_episdode = self.episodes[self.dupes[2]]
-        self.assertEqual("The Agendas Week in Review-3.mp4",
-                         third_duplicate_episdode.basename())
+        self.xassertEqual("The Agendas Week in Review-3.mp4",
+                          third_duplicate_episdode.locallink())
 
     def gather_dupe_indices(self):
         '''
@@ -51,59 +53,68 @@ class DuplicatesTest(unittest.TestCase):
         self.assertEqual("The Agendas Week in Review.mp4", first_dupe.basename())
 
     def test_follower_of_dupe(self):
-        index_of_folower_of_first_dupe = self.dupes[0]+1
+        index_of_folower_of_first_dupe = self.dupes[1]+1
         first_follower = self.episodes[index_of_folower_of_first_dupe]
         self.assertEqual("Ontarios Pothole Pains.mp4", first_follower.basename())
 
     def test_of_locallink(self):
         # make sure I don't break anything while extracting basename from locallink
         first_dupe = self.episodes[self.dupes[0]]
-        expected = '~/podcasts/tvo_the_agenda/The Agendas Week in Review.mp4'
+        expected = 'The Agendas Week in Review.mp4'
         actual = first_dupe.locallink()
-        self.assertEqual(os.path.expanduser(expected), actual)
+        self.xassertEqual(os.path.expanduser(expected), actual)
 
-    def gtest_second_dupe_has_disambiguator(self):
+    def test_second_dupe_has_disambiguator(self):
         second_dupe = self.episodes[self.dupes[1]]
-        self.assertEqual("The Agendas Week in Review-2.mp4",
-                         second_dupe.basename())
+        self.xassertEqual("The Agendas Week in Review-2.mp4",
+                          second_dupe.locallink())
 
-    def test_modifying_link_names(self):
-        # Go through and simulate the writing to disk a big chunk of the episodes--at least
-        # as many so as to include the first 3 occurrences of "Week in Review"
 
-        os.system("rm ~/.podcasts-data/rss/tvo_the_agenda.idx")
-        fs = FileSystem()
-        self.sub.lut.load()
 
-        chunk = self.dupes[3]
-        for i in range(chunk):
-            ep = self.episodes[i]
-            self.simulate_writing_episode_to_disk(ep, fs)
+    def test_the_gamut(self):
+        # set up some episodes with duplicate titles
 
-        first_dupe = self.episodes[self.dupes[0]]
-        second_dupe = self.episodes[self.dupes[1]]
 
-        self.assertEqual("The Agendas Week in Review.mp4", first_dupe.reallink())
-        self.assertEqual("The Agendas Week in Review-2.mp4", second_dupe.reallink())
 
-        # make sure that regular titles' names remain as normal
-        index_of_folower_of_first_dupe = self.dupes[0]+1
-        first_follower = self.episodes[index_of_folower_of_first_dupe]
-        expected = "Ontarios Pothole Pains.mp4" # find out via inspection of RSS file
-        self.assertEqual(expected, first_follower.reallink())
+        episode_titles = """Alfa Bravo Charlie Delta Echo Foxtrot Golf Hotel India Delta Juliett Kilo
+Lima Mike November Oscar Papa Quebec Delta  Romeo Sierra Tango Uniform Victor
+Whiskey X Yankee Zulu""".split()
 
-        self.sub.lut.save()
+        self.assertEqual( episode_titles[3], "Delta")
 
-    def simulate_writing_episode_to_disk(self, episode, fs):
-        table = episode.subscription.lut.table
-        already_used_filenames = []
-        for k in table.keys():
-            already_used_filenames.append( table[k])
-        count = 1
-        proposed = episode.base_sans_extension() + episode._file_extension()
-        while True:
-            if proposed not in already_used_filenames:
-                break
+        episode_objects = []
+        count = 0
+
+        self.standardpath = init_config()
+        subscriptions = Subscriptions(self.standardpath)
+        fs = FakeSubscription(subscriptions, "foobar")
+
+        for e in episode_titles:
+            episode_object = Episode(fs)
+            episode_object.guid = count
+            episode_object.title = e
+            episode_object.url = 'http://www.example.com/foo/bar/baz.mp3'
             count = count + 1
-            proposed = episode.base_sans_extension() + "-%d" % count + episode._file_extension()
-        table[episode.guid] = proposed
+            episode_objects.append( episode_object )
+
+        self.assertEqual( episode_objects[-1].title, "Zulu")
+        self.assertEqual( episode_objects[-1].guid, len(episode_objects)-1)
+
+        fs._fake_episode_list = episode_objects
+        processed = fs.get_all_episodes()
+
+        # call "get_all_episodes" to engage the processing
+        # assert that dupes have modifed episode names
+
+        def yassertEqual(a, b):
+            c = os.path.join(os.path.expanduser("~/podcasts/foobar"), a)
+            d = b
+            self.assertEqual(c, d)
+
+        yassertEqual('Alfa.mp3', processed[0].locallink() )
+        yassertEqual('Delta.mp3', processed[3].locallink() )
+        yassertEqual('Delta-2.mp3', processed[9].locallink() )
+
+        # push some new episodes onto the stream
+        # such that the oldest of maxeps scrolls away
+        # note the oldest dupe scrolls away ( & freeing up one of the dupe name mods )
