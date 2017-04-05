ef18bb7b6ff3919e6a2785453a25d80efd93c440
Tom Billingsley <tom.billingsley@gmail.com>
2017-04-05 07:23:12 -0400

    Add a remove_entry method

--- LookupTable.py
+++ LookupTable.py
@@ -21,13 +21,16 @@ class LookupTable:
             text = open(self.fp, 'r').read()
             self.table = json.loads(text)
 
+    def remove_entry(self, key):
+        if key in self.table:
+            del self.table[key]
+
     def save(self):
         if len(self.table.keys()) > 1:
             s = json.dumps(self.table)
             f = open(self.fp, 'w')
             f.write(s)
             f.close
-
 
 class FakeLookupTable(LookupTable):
     def __init__(self):
