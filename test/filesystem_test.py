a2e5ef0ad3c4915224af7e7bc764cc776cd0d1c2
Tom Billingsley <tom.billingsley@gmail.com>
2020-10-10 15:49:16 -0400

    Make first pass at converting test/filesystem_test to pytest

--- test/filesystem_test.py
+++ test/filesystem_test.py
@@ -1,101 +1,109 @@
 import os
-import unittest
+import pytest 
 import random
 from filesystem import FakeFileSystem
 import logging
 
 logger = logging.getLogger()
-# logger.setLevel(logging.INFO)
-
-class FileSystemTest(unittest.TestCase):
-
-    def _prepare_fake_versions_of_real_files(self):
-        podcastroot = os.path.expanduser("~/podcasts")
-        if os.path.exists(podcastroot):
-            files = os.listdir(podcastroot)
-            dirs = [os.path.join(podcastroot, f) for f in files if
-                    os.path.isdir(os.path.join(podcastroot, f))]
-            rdir = random.choice(dirs)
-            self.podcastdir = rdir
-            files = os.listdir(self.podcastdir)
-            for f in files:
-                self.ffs.touch(self.podcastdir, f)
-
-    def setUp(self):
-        self.ffs = FakeFileSystem()
-        self._prepare_fake_versions_of_real_files()
-
-    def test_fake_listdir(self):
-        if len(self.ffs.listdir(self.podcastdir)) > 0:
-            self.assertEqual(len(os.listdir(self.podcastdir)),
-                             len(self.ffs.listdir(self.podcastdir)))
-
-    def _set_up_(self):
-        self.hypothetical_path = "~/foo-bar/quux.mp3"
-        self.hypothetical_filename = self.hypothetical_path.split("/")[-1]
-        self.hypothetical_dir = "/".join(self.hypothetical_path.split("/")[0:-1])
-
-    def test_exception_from_making_redundant_link(self):
-        with self.assertRaises(ValueError):
-            self._set_up_()
-            self.ffs.mkdir(self.hypothetical_dir)
-            self.ffs.touch(self.hypothetical_dir, self.hypothetical_path)
-            
-            dest = "~/foo-bar/dest.mp3"
-            self.ffs.link(self.hypothetical_path,
-                          dest)
-
-            self.ffs.link(self.hypothetical_path,
-                          dest)
+logger.setLevel(logging.INFO)
 
 
-    def test_fake_file_system_tests(self):
+@pytest.fixture()
+def hypothetical():
+    class Hypothetical:
+        path = "~/foo-bar/quux.mp3"
+        filename = path.split("/")[-1]
+        dir = "/".join(path.split("/")[0:-1])
 
-        self._set_up_()
+    h = Hypothetical()
+    yield h 
 
-        self.assertFalse(self.ffs.path_exists(self.hypothetical_dir))
-        self.assertFalse(self.ffs.path_exists(self.hypothetical_path))
 
-        self.ffs.touch(self.hypothetical_dir, self.hypothetical_filename)
 
-        self.assertEqual(1, len(self.ffs.listdir(self.hypothetical_dir)))
-        self.assertTrue(self.ffs.path_exists(self.hypothetical_path))
+@pytest.fixture()
+def ffs():
+    ffs =  FakeFileSystem()
+    yield ffs
 
-        self.ffs.prune_file(self.hypothetical_path)
 
-        self.assertFalse(self.ffs.path_exists(self.hypothetical_path))
-        self.assertEqual(0, len(self.ffs.listdir(self.hypothetical_dir)))
+@pytest.fixture()
+def podcastdir(ffs):
+    podcastroot = os.path.expanduser("~/podcasts")
+    if os.path.exists(podcastroot):
+        files = os.listdir(podcastroot)
+        dirs = [os.path.join(podcastroot, f)   for f in files if           os.path.isdir(os.path.join(podcastroot, f))]
+        rdir = random.choice(dirs)
+        podcastdir = rdir
+        files = os.listdir(podcastdir)
+        for f in files:
+            ffs.touch(podcastdir, f)
 
-        known_unknown = "~/foo-bar/known_unknown.mp3"
-        self.assertFalse(self.ffs.path_exists(known_unknown))
+    yield podcastdir
 
-    def test_link_creation_test(self):
-        src = '~/.podcasts-data/freakonomics/freakonomics_podcast112917.mp3'
-        dst = '~/podcasts/freakonomics/Are We Running Out of Ideas.mp3 '
-        self.ffs.touch(self.ffs._directory_portion_of_full_path(src),
-                       self.ffs._filename_portion_of_full_path(src))
-        self.assertTrue(self.ffs.path_exists(src))
-        self.assertFalse(self.ffs.path_exists(dst))
-        self.assertTrue(self.ffs.link_creation_test(src, dst))
-        self.ffs.mkdir(self.ffs._directory_portion_of_full_path(dst))
-        self.ffs.link(src,dst)
 
-        self.assertTrue(self.ffs.path_exists(dst))
-        self.assertFalse(self.ffs.link_creation_test(src, dst)) 
-        
-    def test_pathexists_for_nested_empty_directory(self):
-        dir = '/foo/bar'
-        self.ffs.mkdir(dir)
-        self.assertTrue(self.ffs.path_exists(dir))
+
+def test_exception_thrown_on_repeated_call_to_mkdir():
+    pass
+
+
+def test_fake_listdir(ffs, podcastdir):
+    if len(ffs.listdir(podcastdir)) > 0:
+        assert len(os.listdir(podcastdir)) == len(ffs.listdir(podcastdir))
         
 
-    def g_test_file_rename(self):
-        
-        self._set_up_()
-        self.ffs.rename(self.hypothetical_path,
-                        self.ffs.rename(self.hypothetical_path) )
+def test_exception_from_making_redundant_link(ffs, hypothetical):
+    with pytest.raises(ValueError):
+        ffs.mkdir(hypothetical.dir)
+        ffs.touch(hypothetical.dir, hypothetical.path)
+
+        dest = "~/foo-bar/dest.mp3"
+        ffs.link(hypothetical.path,
+                      dest)
+
+        ffs.link(hypothetical.path,
+                      dest)
 
 
+def test_fake_file_system_tests(ffs, hypothetical):
 
-if __name__ == '__main__':
-    unittest.main()
+    assert not ffs.path_exists(hypothetical.dir)
+    assert not ffs.path_exists(hypothetical.path)
+
+    ffs.touch(hypothetical.dir, hypothetical.filename)
+
+    assert(1 == len(ffs.listdir(hypothetical.dir)))
+    assert (ffs.path_exists(hypothetical.path))
+
+    ffs.prune_file(hypothetical.path)
+
+    assert not (ffs.path_exists(hypothetical.path))
+    assert (0 == len(ffs.listdir(hypothetical.dir)))
+
+    known_unknown = "~/foo-bar/known_unknown.mp3"
+    assert not (ffs.path_exists(known_unknown))
+
+
+def test_link_creation_test(ffs):
+    src = '~/.podcasts-data/freakonomics/freakonomics_podcast112917.mp3'
+    dst = '~/podcasts/freakonomics/Are We Running Out of Ideas.mp3 '
+    ffs.touch(ffs._directory_portion_of_full_path(src),
+                   ffs._filename_portion_of_full_path(src))
+    assert (ffs.path_exists(src))
+    assert not (ffs.path_exists(dst))
+    assert (ffs.link_creation_test(src, dst))
+    ffs.mkdir(ffs._directory_portion_of_full_path(dst))
+    ffs.link(src,dst)
+
+    assert (ffs.path_exists(dst))
+    assert not (ffs.link_creation_test(src, dst)) 
+
+
+def test_pathexists_for_nested_empty_directory(ffs):
+    dir = '/foo/bar'
+    ffs.mkdir(dir)
+    assert (ffs.path_exists(dir))
+
+@pytest.mark.skip(reason="")
+def test_file_rename(ffs, hypothetical):
+    ffs.rename(hypothetical.path,
+                    ffs.rename(hypothetical.path) )
