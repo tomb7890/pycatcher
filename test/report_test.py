115d80cc5bbaa4e0b50bc51a0be5e3967c3887ca
Tom Billingsley <tom.billingsley@gmail.com>
2020-10-15 11:09:50 -0400

    Get report.enumerate_all_downloaded_episodes under test

--- test/report_test.py
+++ test/report_test.py
@@ -121,10 +121,11 @@ def test_enumerate_all_downloaded_episodes():
                 return True
             return False
 
+    section = "0xdeadbeef"
     subscriptions = gather_all_subscriptions()
     for s in subscriptions:
         db = MyFakeIndex()
 
         reportdata = []
-        enumerate_all_downloaded_episodes(s, db, reportdata)
+        enumerate_all_downloaded_episodes(s, db, section, reportdata)
         assert len(reportdata) == ARBITRARY_NUMBER
