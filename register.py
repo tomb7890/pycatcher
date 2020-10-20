1928a20c0963463077186c8a212535e5a1b84913
Tom Billingsley <tom.billingsley@gmail.com>
2020-10-20 13:49:36 -0400

    Delete dead registration code

--- register.py
+++ register.py
@@ -8,31 +8,10 @@ from configparser import ConfigParser, DuplicateSectionError
 def register(feedurl, title):
     try:
         _setup_config(feedurl, title)
-        # _create_base_directories()
-        # _create_subscription_directories(title)
-        # _download_rss_file(feedurl, title)
     except DuplicateSectionError as e:
         print("Error adding %s: %s. " % (title, e))
 
-
-def _create_base_directories():
-    dirs = [podcasts_directory(), data_directory(), rss_directory()]
-    for d in dirs:
-        if not os.path.exists(d):
-            os.mkdir(d)
-
-def _create_subscription_directories(subscription):
-    subscription_directories = [
-        os.path.join(podcasts_directory(), _media_subdirectory(subscription)),
-        os.path.join(data_directory(), _media_subdirectory(subscription))
-    ]
-
-    for d in subscription_directories:
-        if not os.path.exists(d):
-            os.mkdir(d)
-
 def _setup_config(feedurl, title):
-
     cp = ConfigParser()
     cp.read(DEFAULTCONFIGFILE)
     section = title
